10b647d4d62d097ac36d2fe5201f63b2b235679a
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-09 11:23:46 -0300

    feat: add fetch veggie's  from api and add styles

--- src/components/Veggie/veggie.component.tsx
+++ src/components/Veggie/veggie.component.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+//hooks
+import { useState, useEffect } from 'react'
+
+//splide
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
+
+//styles
+import { Card, Gradient, Wrapper } from './veggie.styles'
 
 const Veggie = () => {
+
+  
+  const [veggie, setVeggie] = useState<string[]>([])
+
+  useEffect(() => {
+    getVeggie()
+  }, [])
+
+  const getVeggie = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=4e3f18fbe0c64fcea715c32a8163f177&number=9&tags=vegetarian`);
+    const data = await api.json();
+    console.log(data.recipes)
+    setVeggie(data.recipes)
+  }
   return (
-    <div>Veggie</div>
+    <Wrapper>
+      <h3>Vegetarian Picks</h3>
+
+      <Splide options={{
+        perPage: 3,
+        arrows: false,
+        pagination: false,
+        drag: 'free',
+        gap: '5rem'
+      }}>
+        {veggie.map((recipe: any) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Card key={recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+                <Gradient/>
+              </Card>
+            </SplideSlide>
+          )
+        })}
+      </Splide>
+    </Wrapper>
   )
 }
 
