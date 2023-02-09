7cd7354b95a38c1502d32ef3ec53263f6ceb9790
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-09 15:37:59 -0300

    feat: add path and element of pages

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,11 +1,29 @@
-import { useEffect, useState } from "react";
-import Home from "./Home"
+//routes
+import { Routes, Route, useLocation } from 'react-router-dom'
+
+//pages
+import Home from "./Home/Home"
+import { Cuisine } from "./Cuisine/Cuisine"
+import { Search } from "./Search/Search"
+import { Recipe } from "./Recipe/Recipe"
+
+//framer motion
+import {AnimatePresence} from 'framer-motion'
 
 const Pages = () => {
 
+  const location = useLocation()
+
   return (
     <div>
-        <Home/>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cuisine/:type" element={<Cuisine/>}/>
+          <Route path="/searched/:search" element={<Search/>}/>
+          <Route path="/recipe/:name" element={<Recipe/>}/>
+        </Routes>
+      </AnimatePresence>
     </div>
   )
 }
