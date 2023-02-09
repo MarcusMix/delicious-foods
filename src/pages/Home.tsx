32f5c61e3280833f39192400fdb25b457c36350d
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-08 22:22:40 -0300

    feat: add home page and popular & veggie component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+//pages
+import Popular from "../components/Popular/popular.component"
+import Veggie from "../components/Veggie/veggie.component"
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            <Popular/>
+            <Veggie/>
+        </div>
+    )
+}
+
+export default Home
