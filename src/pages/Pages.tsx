0c1aadd7a9efadeb50670aa2144a028334105b09
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-08 22:23:07 -0300

    feat: create pages and add home

---
+++ src/pages/Pages.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import Home from "./Home"
+
+const Pages = () => {
+
+  
+
+  return (
+    <div>
+        <Home/>
+    </div>
+  )
+}
+
+export default Pages
